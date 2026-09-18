2467281ca707b1cb1c647fc03fa949f9ccd1ab2c
simpleonly <2517077936@qq.com>
2026-09-18 14:13:42 +0800

    Update binary target URL for SimpleToastSDK

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "simpleToast",
-            url: "https://github.com/你的用户名/MySDK/releases/download/1.0.0/MySDK.zip",
+            url: "https://github.com/simpleonly/toast/releases/download/1.0.0/SimpleToastSDK.xcframework.zip",
             checksum: "67ade0b58b18b846de711a5334284efa19378ffdd419d5016cd572c73f62e6b1"
         )
     ]
