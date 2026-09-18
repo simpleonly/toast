f99f45d47960fabd7fbc8f3f6ef492254e07df8f
simpleonly <2517077936@qq.com>
2026-09-18 14:06:46 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "simpleToast",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "simpleToast", targets: ["simpleToast"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "simpleToast",
+            url: "https://github.com/你的用户名/MySDK/releases/download/1.0.0/MySDK.zip",
+            checksum: "67ade0b58b18b846de711a5334284efa19378ffdd419d5016cd572c73f62e6b1"
+        )
+    ]
+)
